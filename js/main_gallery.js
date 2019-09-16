15cb25cdfc20b61d519c635123eec465c2b3bb3f
fuchsia-84 <mikitech9084@gmail.com>
2019-09-16 16:50:34 +0900

    add flickity cdn link, script files.

---
+++ js/main_gallery.js
@@ -0,0 +1,6 @@
+$(function() {
+    $('.main_gallery').flickity({
+        autoPlay: true,
+        draggble: false
+    });
+});
---
+++ js/main_gallery_tab.js
@@ -0,0 +1,6 @@
+$(function() {
+    $('.main_gallery_tab').flickity({
+        autoPlay: false,
+        draggble: true
+    });
+});
