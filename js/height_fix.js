d9aa681e6317f586320007097ac0a2d052603dc7
fuchsia-84 <mikitech9084@gmail.com>
2019-09-30 23:09:16 +0900

    make fix height responsible, and fix top height.

--- js/height_fix.js
+++ js/height_fix.js
@@ -1,4 +1,15 @@
-$(function() {
-    var height=$("#header").height();
-    $(".content").css("padding-top", height);//10pxだけ余裕をもたせる
+var $win = $(window);
+
+$win.on('load resize', function() {
+
+    var windowWidth = window.innerWidth;
+    var header_height=$("#header").height();
+    var header_innner_height=$("#header .inner").height();
+    var header_sp_height=$("#header_sp").height();
+
+    if(windowWidth > 1024) {
+        $("#content").css("padding-top", header_height + header_innner_height);
+    } else {
+        $("#content").css("padding-top", header_sp_height);
+    }
 });
